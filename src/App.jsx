cd636824761730e44a7926dc0449f949256dbf20
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-02-02 02:37:50 +0500

    feat: initialize gradient generator project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created main application component (App.jsx) with state management for gradient parameters.
    - Added SVG assets for Vite and React logos.
    - Configured Vite with React and Tailwind CSS plugins.
    - Created index.css for Tailwind CSS imports.
    - Set up main entry point (main.jsx) for rendering the application.

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [num, setNum] = useState(12);
+  const [type, setType] = useState("Linear");
+
+  return (
+    <div className="min-h-screen py-12">
+      <div className="w-9/12 mx-auto">
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-bold">
+            🟧 Gradient Generator - ({num}) ({type})
+          </h1>
+          <div className="flex gap-4">
+            <input
+              className="border border-slate-300 bg-white rounded-lg w-[100px] p-2 placeholder-gray-400"
+              placeholder="12"
+              onChange={(e) => setNum(Number(e.target.value))}
+            />
+
+            <select
+              value={type}
+              className="border border-slate-300 bg-white rounded-lg w-[100px] p-2"
+              onChange={(e) => setType(e.target.value)}
+            >
+              <option value="Linear">Linear</option>
+              <option value="Radial">Radial</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      ``
+    </div>
+  );
+};
+
+export default App;
